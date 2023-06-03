1372f163e238e537afee5e35d505500cc30268c8
ねず <reborn.dance1985@gmail.com>
2023-06-03 09:42:30 +0900

    新規投稿ページへ遷移するよう

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -10,4 +10,18 @@ class BoardsController < ApplicationController
   def new
     @board = Board.new
   end
+
+  def create
+    @board = current_user.boards.build(board_params)
+    if @board.save
+      redirect_to board_path(@board)
+    else
+      render :new
+    end
+  end
+
+  private
+  def board_params
+    params.require(:board).permit(:name, :description)
+  end
 end
