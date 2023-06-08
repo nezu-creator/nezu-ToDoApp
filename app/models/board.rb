1f35c4c155db209237a80ce8ff5ee35ef85ddd02
ねず <reborn.dance1985@gmail.com>
2023-06-08 14:26:21 +0900

    テーブルのuser_idを作り直した。（add_reference)

--- app/models/board.rb
+++ app/models/board.rb
@@ -7,7 +7,7 @@
 #  name        :string           not null
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
-#  user_id     :bigint           not null
+#  user_id     :bigint
 #
 # Indexes
 #
--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_30_054435) do
+ActiveRecord::Schema.define(version: 2023_06_08_052136) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "boards", force: :cascade do |t|
-    t.bigint "user_id", null: false
     t.string "name", null: false
     t.text "description", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id"
     t.index ["user_id"], name: "index_boards_on_user_id"
   end
 
