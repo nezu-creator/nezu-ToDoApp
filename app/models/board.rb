477301b3cbef0bc50a3e18ce691734484a7a9e46
ねず <reborn.dance1985@gmail.com>
2023-06-09 09:25:17 +0900

    掲示板投稿者の名前を表示

--- app/models/board.rb
+++ app/models/board.rb
@@ -15,11 +15,12 @@
 #
 class Board < ApplicationRecord
 
-  belongs_to :user
 
   validates :name, presence: true
   validates :description, presence: true
   validates :description, uniqueness: true
+
+  belongs_to :user
 
   def display_created_at
     I18n.l(self.created_at, format: :default)
--- db/migrate/20230608235641_add_user_id_to_board.rb
+++ db/migrate/20230608235641_add_user_id_to_board.rb
@@ -1,4 +1,4 @@
-class AddUserIdToBoards < ActiveRecord::Migration[6.0]
+class AddUserIdToBoard < ActiveRecord::Migration[6.0]
   def change
     add_reference :boards, :user
   end
