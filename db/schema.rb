f333cbfe20800741547e295cc4069433321a82fc
ねず <reborn.dance1985@gmail.com>
2023-06-14 09:53:28 +0900

    アイキャッチの設定、時間を東京へ設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_12_022127) do
+ActiveRecord::Schema.define(version: 2023_06_12_195841) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 2023_06_12_022127) do
     t.string "name"
     t.text "description"
     t.bigint "user_id"
+    t.date "deadline"
     t.index ["board_id"], name: "index_tasks_on_board_id"
     t.index ["user_id"], name: "index_tasks_on_user_id"
   end
