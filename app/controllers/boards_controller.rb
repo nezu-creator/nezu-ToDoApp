c96ec75b4975e3a92acda3da84ef736632b8b593
ねず <reborn.dance1985@gmail.com>
2023-06-24 05:52:32 +0900

    見た目の調整

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -16,7 +16,7 @@ class BoardsController < ApplicationController
   def create
     @board = current_user.boards.build(board_params)
     if @board.save
-      redirect_to board_path(@board), notice: '保存できました'
+      redirect_to root_path, notice: '保存できました'
     else
       flash.now[:error] = '保存できませんでした'
       render :new
